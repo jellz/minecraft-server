5dde75a72d841681603b24f3aac01fa7863a8406
Daniel Gulic <danielgulic@gmail.com>
2019-06-22 06:23:29 +1000

    onLogin isn't being fired :(

--- src/plugins/LoginPlugin.js
+++ src/plugins/LoginPlugin.js
@@ -1,14 +1,31 @@
-const { Plugin }  = require('../Plugin');
+const { Plugin } = require('../Plugin');
 
 class LoginPlugin extends Plugin {
   constructor(server) {
     super(server);
 
-    this.server.on('login', this.onLogin);
+    this.server.on('login', (client) => this.onLogin(client));
   }
 
   onLogin(client) {
     console.log('someone logged in');
+    client.write('login', {
+      entityId: client.id,
+      levelType: 'default',
+      gameMode: 1,
+      dimension: 0,
+      difficulty: 2,
+      maxPlayers: this.server.maxPlayers,
+      reducedDebugInfo: false
+    });
+    client.write('position', {
+      x: 0,
+      y: 1.62,
+      z: 0,
+      yaw: 0,
+      pitch: 0,
+      flags: 0x00
+    });
   }
 }
 
