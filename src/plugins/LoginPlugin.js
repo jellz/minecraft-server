c3fa44f79c9dc316ab286a0326b05ac8931a42e7
Daniel Gulic <danielgulic@gmail.com>
2019-06-22 06:06:55 +1000

    login plugin

--- src/plugins/LoginPlugin.js
+++ src/plugins/LoginPlugin.js
@@ -3,6 +3,11 @@ const Plugin = require('../Plugin');
 class LoginPlugin extends Plugin {
   constructor(server) {
     super(server);
+
+    this.server.on('login', this.onLogin);
   }
 
+  onLogin(client) {
+    console.log('someone logged in');
+  }
 }
