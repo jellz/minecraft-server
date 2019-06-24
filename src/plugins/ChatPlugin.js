21f8001273d178baaf5e68db73d3a0b402214f20
Daniel Gulic <danielgulic@gmail.com>
2019-06-24 19:21:53 +1000

    Chat plugin!

---
+++ src/plugins/ChatPlugin.js
@@ -0,0 +1,34 @@
+const Plugin = require('../Plugin');
+
+class ChatPlugin extends Plugin {
+  constructor(server) {
+    super(server);
+    this.server.on('chat', (player, message) => {
+      Object.values(this.server.server.clients).forEach(_client => {
+        _client.write('chat', {
+          message: JSON.stringify({
+            translate: 'chat.type.text',
+            with: [
+              {
+                text: player.username,
+                clickEvent: {
+                  action: 'suggest_command',
+                  value: `/msg ${player.username} `
+                },
+                hoverEvent: {
+                  action: 'show_entity',
+                  value: `{id:${player.uuid},name:${player.username}}`
+                },
+                insertion: player.username
+              },
+              { text: message }
+            ]
+          }),
+          position: 0
+        });
+      });
+    });
+  }
+}
+
+module.exports = ChatPlugin;
