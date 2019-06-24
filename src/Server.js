57e3ac8fac3b57e619d70c684d45cece50cba0da
Daniel Gulic <danielgulic@gmail.com>
2019-06-24 23:23:34 +1000

    more moving of Util.js

--- src/Server.js
+++ src/Server.js
@@ -1,6 +1,6 @@
 const mc = require('minecraft-protocol');
 const Player = require('./Player');
-const { parseColoredMessage } = require('./Util');
+const { parseColoredMessage } = require('./util/Util');
 const EventEmitter = require('events');
 
 class Server extends EventEmitter {
